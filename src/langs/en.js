0920ebe1a2cfe47a8afcdcd61c5264dffe987ff3
somebeaver <admin@somebeaver.ca>
2021-04-13 16:12:36 -0400

    edits

--- src/langs/en.js
+++ src/langs/en.js
@@ -95,13 +95,14 @@ module.exports = {
   "server-connect.logo-title":                                            "Chrip chirp",
   "server-connect.help-c2a":                                              "How do I connect?",
   "server-connect.help-body":                                             `
-                                                                          <p>To use this app, you must have <a href="https://cardinalapp.xyz/cardinal-server" class="external">Cardinal Server</a> running somewhere on your network.</p>
-                                                                          <p>Once the server is running, the location information can be found in the <strong>Status</strong> widget, and it will look something like,</p>
+                                                                          <p>To use this app, you must have <a href="https://cardinalapp.xyz/cardinal-server" class="external">Cardinal Server</a> running somewhere on your local area network.</p>
+                                                                          <p>Once the server is running, the location information can be found in the server's <strong>Status</strong> widget, and it will look something like:</p>
                                                                           <code>192.168.0.0:0000</code>
                                                                           <p>The <strong>Host</strong> is the part to the left of the colon, usually an IP address.</p>
-                                                                          <p>The <strong>Port</strong> is the part to the right of the colon, it will a number with up to 5 digits.</p>
+                                                                          <p>The <strong>Port</strong> is the part to the right of the colon, it is a number with up to 5 digits.</p>
                                                                           <p>The server that you connect to must continue running while using any client app.</p>
                                                                           `,
+  "server-connect.connection-failed":                                     "Connection failed",
   
   // self updater
   "updater.ask-to-update.title":                                          "Update available",
