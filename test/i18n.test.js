6e8da8af2033f4ef82197dba96865072e9e7b77b
Kevin Kandlbinder <kevin@kevink.dev>
2021-06-27 19:15:44 +0200

    Update i18n.test.js

--- test/i18n.test.js
+++ test/i18n.test.js
@@ -18,3 +18,12 @@ tap.test('Returns the French test string', async (t) => {
   
   return t.equal(returnedString, 'French')
 })
+
+/**
+ * German
+ */
+tap.test('Returns the German test string', async (t) => {
+  let returnedString = i18n.string('unit-test-string', 'de')
+  
+  return t.equal(returnedString, 'German')
+})
